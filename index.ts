85c2adcfa499498cfab73f277efe6c32097588c2
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-08-13 11:41:13 +0600

    main function to start program

--- index.ts
+++ index.ts
@@ -25,7 +25,10 @@ async function main() {
 
     if (currentPageIndex === 1) {
       totalAdsCount = getTotalAdsCount($);
+<<<<<<< HEAD
       console.log(totalAdsCount);
+=======
+>>>>>>> c8d9f73 (main function to start program)
     }
 
     const items = addItems($);
@@ -61,6 +64,7 @@ async function getNextPageUrl($: CheerioAPI) {
       data: allAds,
     };
 
+<<<<<<< HEAD
     fs.writeFile('ads.json', JSON.stringify(data, null, 2), 'utf-8', (err) => {
       if (err) {
         console.error(err);
@@ -69,6 +73,20 @@ async function getNextPageUrl($: CheerioAPI) {
       }
     });
     return;
+=======
+    fs.writeFile(
+      'result.json',
+      JSON.stringify(data, null, 2),
+      'utf-8',
+      (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('File Saved!');
+        }
+      }
+    );
+>>>>>>> c8d9f73 (main function to start program)
   }
 
   await main();
